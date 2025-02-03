9b0f411ec924bc10fae1c54498edca7456973164
Bogdan Dogaru <bogdan@vevolmedia.com>
2025-02-03 17:12:44 +0200

    small update

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet-async';
+import ogImage from '../assets/og-v1.png';
 
 export default function SEO({ title, description, image = 'https://mideally.com/default-og.jpg', url }) {
 	const siteTitle = 'Mideally - Conectăm clienții cu business-urile';
@@ -13,14 +14,14 @@ export default function SEO({ title, description, image = 'https://mideally.com/
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={`${title} | ${siteTitle}`} />
 			<meta property="og:description" content={description} />
-			<meta property="og:image" content={image} />
+			<meta property="og:image" content={ogImage} />
 			{url && <meta property="og:url" content={url} />}
 
 			{/* Twitter */}
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:title" content={`${title} | ${siteTitle}`} />
 			<meta name="twitter:description" content={description} />
-			<meta name="twitter:image" content={image} />
+			<meta name="twitter:image" content={ogImage} />
 
 			{/* Canonical URL */}
 			{url && <link rel="canonical" href={url} />}
