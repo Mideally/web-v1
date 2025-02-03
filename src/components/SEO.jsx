3f54b73ab0335450f24e6591c4e6f0145f4bc189
Bogdan Dogaru <bogdan@vevolmedia.com>
2025-02-03 17:16:12 +0200

    small update

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -14,14 +14,14 @@ export default function SEO({ title, description, image = 'https://mideally.com/
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={`${title} | ${siteTitle}`} />
 			<meta property="og:description" content={description} />
-			<meta property="og:image" content={ogImage} />
+			<meta property="og:image" content={`https://mideally.com/${ogImage}`} />
 			{url && <meta property="og:url" content={url} />}
 
 			{/* Twitter */}
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:title" content={`${title} | ${siteTitle}`} />
 			<meta name="twitter:description" content={description} />
-			<meta name="twitter:image" content={ogImage} />
+			<meta name="twitter:image" content={`https://mideally.com/${ogImage}`} />
 
 			{/* Canonical URL */}
 			{url && <link rel="canonical" href={url} />}
